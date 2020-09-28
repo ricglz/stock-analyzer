23c195a555c26ffe37edd61bcd2ef177e6c80894
Ricardo J González C <rjgcastillo@hotmail.com>
2020-09-28 12:28:17 -0500

    Pull data just once per day and save the data for the day

--- src/stock.py
+++ src/stock.py
@@ -3,37 +3,51 @@
 """
 Module in charge of managing the stock and its analystic values
 """
-from datetime import datetime
+from datetime import date, timedelta
+from os import stat
 
 import matplotlib.dates as mdates
-import pandas_datareader.data as web
-from pandas import DataFrame
+from pandas_datareader.data import get_data_yahoo
+from pandas import DataFrame, read_csv
 
 from ema import calculate_ema
 from macd import calculate_macd
 from rsi import calculate_rsi
 from sma import calculate_sma
 
+def get_stock_data(ticker, start, end):
+    last_modified = None
+    stock_file = 'csvs/history/{}.csv'.format(ticker)
+    today = date.today()
+    try:
+        last_modified = date.fromtimestamp(stat(stock_file).st_mtime)
+    except FileNotFoundError:
+        last_modified = today - timedelta(days=1)
+    stock_data = None
+    if last_modified < today:
+        stock_data = get_data_yahoo(ticker, start, end)
+        stock_data.to_csv(stock_file)
+    else:
+        stock_data = read_csv(stock_file)
+    return stock_data
 
 class Stock:
     closes = None
-    dates = None
     macd = None
     rsi = None
     signal_line = None
 
 
-    def __init__(self, ticker, start, end=datetime.now()):
+    def __init__(self, ticker, start, end):
         """
         Different sources for pulling data can be found here:
         https://readthedocs.org/projects/pandas-datareader/downloads/pdf/latest/
         """
-        stock_data = web.DataReader(ticker, 'yahoo', start, end)
+        stock_data = get_stock_data(ticker, start, end)
 
         self.ticker = ticker
-        self.dates = [mdates.date2num(d) for d in stock_data.index]
-        self.closes = stock_data['Close']
-        self.closes_dt = DataFrame(self.closes.array)
+        self.closes = stock_data['Close'].tolist()
+        self.closes_dt = DataFrame(self.closes)
 
         self.macd = calculate_macd(self.closes_dt).values.flatten().tolist()
         self.rsi = calculate_rsi(self.closes)
